3504c18f04bb28637ddac881c99370c1dd918875
Kristopher Santos <topers1820@gmail.com>
2025-05-08 17:26:24 +0800

    Setup mantine ui

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { Card, Container } from "@mantine/core";
+
+export default function Login() {
+  return (
+    <Container size="md" className="h-screen grid place-content-center">
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        Doggy
+      </Card>
+    </Container>
+  );
+}
