b58a41c1b4bf69e774ddbebfcd5a0ebdb803953b
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-05-11 21:15:34 +0800

    Updated llms service to allow switching between lms

--- backend/app/services/llm_service.ts
+++ backend/app/services/llm_service.ts
@@ -1,8 +1,52 @@
 import env from '#start/env'
-import { GoogleGenAI } from '@google/genai'
+import { inject } from '@adonisjs/core'
+import { GoogleGenAI, createUserContent, createPartFromUri } from '@google/genai'
 
+export enum LLM {
+  GEMINI,
+}
+
+@inject()
 export default class LLMService {
+  static LLM: any
   gemini() {
     return new GoogleGenAI({ apiKey: env.get('GEMINI_KEY') })
+  }
+
+  constructor(private llm: LLM) {}
+  client() {
+    switch (this.llm) {
+      case LLM.GEMINI:
+        return new GoogleGenAI({ apiKey: env.get('GEMINI_KEY') })
+      default:
+        throw new Error('LLM not supported')
+    }
+  }
+
+  async invoke(prompt: string, attachmentUrls: string[]) {
+    switch (this.llm) {
+      case LLM.GEMINI:
+        const attachments = await Promise.all(
+          attachmentUrls.map((image) =>
+            this.client().files.upload({
+              file: image,
+            })
+          )
+        )
+
+        const response = await this.client().models.generateContent({
+          model: 'gemini-2.0-flash',
+          contents: createUserContent([
+            prompt,
+            ...attachments.map((attachment) =>
+              createPartFromUri(attachment.uri ?? '', attachment.mimeType ?? '')
+            ),
+          ]),
+        })
+
+        return {
+          response: response,
+        }
+    }
   }
 }
