213812d841c021e106ae049f5c0989ed7dd3aadd
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-05-12 21:00:27 +0800

    Create gemini_configs.ts
    - gemini response generator configs
    - based on templates

---
+++ backend/app/util/gemini_configs.ts
@@ -0,0 +1,106 @@
+import { Topic } from '#models/chat'
+import { GenerateContentConfig, Modality, Type } from '@google/genai'
+import { Template } from './template.js'
+
+export default class GeminiConfigs {
+  private default: GenerateContentConfig = {
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.OBJECT,
+      properties: {
+        response: { type: Type.STRING },
+      },
+    },
+  }
+
+  private generate_title: GenerateContentConfig = {
+    maxOutputTokens: 256,
+    temperature: 0.5,
+    systemInstruction:
+      'You are a tutorial assistant. Generate a title (short and catchy) for the following text. And choose the right topic.',
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.ARRAY,
+      items: {
+        type: Type.OBJECT,
+        properties: {
+          title: { type: Type.STRING },
+          topic: { type: Type.STRING, enum: Object.values(Topic) },
+        },
+      },
+    },
+  }
+
+  private generate_image: GenerateContentConfig = {
+    responseModalities: [Modality.TEXT, Modality.IMAGE],
+  }
+
+  private summarize: GenerateContentConfig = {
+    maxOutputTokens: 256,
+    temperature: 0.5,
+    systemInstruction: 'You are a tutorial assistant. Summarize the following text.',
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.OBJECT,
+      properties: {
+        response: { type: Type.STRING },
+      },
+    },
+  }
+
+  private explain_like_im_5: GenerateContentConfig = {
+    maxOutputTokens: 256,
+    temperature: 0.5,
+    systemInstruction:
+      'You are a tutorial assistant. Explain the following text like I am a 5 year old.',
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.OBJECT,
+      properties: {
+        response: { type: Type.STRING },
+      },
+    },
+  }
+
+  private multiple_choice_question: GenerateContentConfig = {
+    maxOutputTokens: 256,
+    temperature: 0.5,
+    systemInstruction:
+      'You are a tutorial assistant. Generate a multiple choice question with 4 options based on the following text.',
+    responseMimeType: 'application/json',
+    responseSchema: {
+      type: Type.ARRAY,
+      items: {
+        type: Type.OBJECT,
+        properties: {
+          question: { type: Type.STRING },
+          answer: { type: Type.STRING },
+          options: { type: Type.ARRAY, items: { type: Type.STRING } },
+        },
+      },
+    },
+  }
+
+  static build() {
+    return new GeminiConfigs()
+  }
+
+  public get(type: Template): GenerateContentConfig {
+    switch (type) {
+      case Template.DEFAULT:
+        return this.default
+      case Template.SUMMARIZE:
+        return this.summarize
+      case Template.GENERATE_TITLE:
+        return this.generate_title
+      case Template.GENERATE_IMAGE:
+        return this.generate_image
+      case Template.EXPLAIN_LIKE_IM_5:
+        return this.explain_like_im_5
+      case Template.MULTIPLE_CHOICE_QUESTION:
+        return this.multiple_choice_question
+      default:
+        return this.default
+    }
+  }
+}
