8aad149a774934b0f56358c2361d45380c219261
Morfusee <markvalenzuela.coding@gmail.com>
2025-05-23 20:30:05 +0800

    TEST: add test case for token verification, and all chat HTTP methods

--- backend/tests/functional/users/list.spec.ts
+++ backend/tests/functional/users/list.spec.ts
@@ -1,8 +1,22 @@
+import { ApiClient } from '@japa/api-client'
 import { test } from '@japa/runner'
 
 const userCreds = {
   email: 'test@test.com',
   password: '123456',
+  firstName: 'Mark',
+  lastName: 'Ngo',
+}
+
+const dummyChat = {
+  prompt: 'What is the best place to travel to?',
+  attachmentUrls: [''],
+  template: 'summarize',
+}
+
+async function loginAndGetCookie(client: ApiClient) {
+  const response = await client.post('/login').json(userCreds)
+  return response.header('set-cookie')?.[0] ?? ''
 }
 
 test.group('Authentication', () => {
@@ -20,18 +34,67 @@ test.group('Authentication', () => {
 })
 
 test.group('Token Verification', () => {
-  // const client = new ApiClient()
-
-  // ApiClient.setup(async (req) => {
-  //   if (req.du)
-
-  //   const response = await client.post('/login').json(userCreds)
-  //   const cookie = response.header('set-cookie')
-  //   if (cookie) req.header('cookie', cookie)
-  // })
-
   test('Get me details', async ({ client }) => {
-    const response = await client.get('/me')
+    // Get the cookie from the response
+    const cookie = await loginAndGetCookie(client)
+
+    // Attach it to the request's header
+    const response = await client.get('/me').header('cookie', cookie)
+
+    response.assertAgainstApiSpec()
+  })
+})
+
+test.group('Chat Functionality Tests', (group) => {
+  let cookie: string
+  let chatId: string
+
+  group.each.setup(async ({ context }) => {
+    const { client } = context
+    cookie = await loginAndGetCookie(client)
+  })
+
+  test('Create a new chat with prompt and template', async ({ client }) => {
+    // Attach it to the request's header
+    const response = await client.post('/chats').header('cookie', cookie).json(dummyChat)
+
+    // Save the chatId for the test group
+    chatId = response.body().chat.id
+
+    response.assertAgainstApiSpec()
+  })
+
+  test('Retrieve all saved chat entries', async ({ client }) => {
+    const response = await client.get('/chats').header('cookie', cookie)
+
+    response.assertAgainstApiSpec()
+  })
+
+  test('Retrieve chat entry by ID', async ({ client }) => {
+    const response = await client.get('/chats/' + chatId).header('cookie', cookie)
+
+    response.assertAgainstApiSpec()
+  })
+
+  test('Update chat name by ID', async ({ client }) => {
+    const newName = 'Updated Name'
+    const response = await client
+      .patch('/chats/' + chatId)
+      .header('cookie', cookie)
+      .json({
+        name: newName,
+      })
+
+    // Check if the name is truly updated
+    response.assertBodyContains({
+      name: newName,
+    })
+
+    response.assertAgainstApiSpec()
+  })
+
+  test('Delete chat entry by ID', async ({ client }) => {
+    const response = await client.delete('/chats/' + chatId).header('cookie', cookie)
 
     response.assertAgainstApiSpec()
   })
