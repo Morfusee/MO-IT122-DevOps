77c798d9c57dce7d21fd4177ecdcbe492ac75003
Morfuse <markvalenzuela.coding@gmail.com>
2025-06-10 10:37:22 +0000

    FIX: Env var doesn't match what's on .env file

--- frontend/lib/server-init.ts
+++ frontend/lib/server-init.ts
@@ -2,7 +2,7 @@ import nextConfig from "@/next.config";
 import { client } from "./client/client.gen";
 
 client.setConfig({
-  baseUrl: nextConfig.env?.API_BASE_URL_SERVER,
+  baseUrl: nextConfig.env?.NEXT_PUBLIC_API_BASE_URL_SERVER,
 });
 
 export { client };
